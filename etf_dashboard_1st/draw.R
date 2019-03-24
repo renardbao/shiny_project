df186f38c32bf692a29348e17b25571fd3830c1f
renardbao <charlie602102002@hotmail.com>
2019-03-24 20:50:01 +0800

    etf dashboard

---
+++ etf_dashboard_1st/draw.R
@@ -0,0 +1,227 @@
+source("etf.R")
+
+#將資料便形成長資料來方便畫圖----
+# etf.data.melt <- list()
+# for (i in 1:length(etf.code)){
+#   etf.data.melt[[paste0("etf",etf.code[i])]] <- 
+#     data.frame(
+#       date = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% "["(,"date") ,
+#       open = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"open") %>% scale(),
+#       close = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"close") %>% scale(),
+#       high = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"high") %>% scale() ,
+#       low =  merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>%
+#         "["(,"low") %>% scale(),
+#       twi = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"twi") %>% scale(),
+#       sp500 = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"sp500") %>% scale(),
+#       brent = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"brent") %>% scale() ,
+#       change.rate = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#         merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#         "["(,"change.rate") %>% scale()
+#       
+#     )  %>% melt(id.vars = "date")
+#   
+#   
+# }
+
+etf.data.melt <- list()
+for (i in 1:length(etf.code)){
+  etf.data.melt[[paste0("etf",etf.code[i])]] <- 
+    data.frame(
+      date = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% "["(,"date") ,
+      open = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"open"),
+      close = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"close") ,
+      high = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"high") ,
+      low =  merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>%
+        "["(,"low") ,
+      twi = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"twi") ,
+      sp500 = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"sp500"),
+      brent = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"brent") ,
+      change.rate = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+        merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+        "["(,"change.rate")
+      
+    )  %>% melt(id.vars = "date") %>% group_by(variable)%>% mutate(scale_value = scale(value)) %>% 
+    as.data.frame()
+  
+  
+}
+
+
+
+ui <- 
+  fluidPage(
+    tags$head(
+      #不顯示error
+      tags$style(type="text/css",
+                 ".shiny-output-error { visibility: hidden; }",
+                 ".shiny-output-error:before { visibility: hidden; }"
+      ),
+      tags$style(
+        "label{
+        color: white;
+        font-size: 30px;
+        font-weight: bold;
+        font-family: Microsoft JhengHei;
+        }"
+      ),
+      tags$style(
+        ".irs-from, .irs-to, .irs-single{
+               font-size: 20px;
+               font-weight: bold;
+               font-family: Microsoft JhengHei;
+        }"
+      ),
+      tags$style(
+        ".help-block{
+               font-size: 14px;
+               font-weight: bold;
+               font-family: Microsoft JhengHei;
+               color:white;
+        }"
+      ),
+      tags$style(
+        ".selectize-control{
+               font-size: 20px;
+               font-weight: bold;
+               font-family: Microsoft JhengHei;
+               
+        }"
+      ),
+      tags$style(
+        ".checkbox label{
+               font-size: 20px;
+               font-weight: bold;
+               font-family: Microsoft JhengHei;
+               
+        }"
+      ),
+      tags$style(
+        ".h2,h2{
+               font-size: 40px;
+               font-weight: bold;
+               font-family: Microsoft JhengHei;
+               color:white;
+               
+        }"
+      )
+      
+    ),
+    setBackgroundColor(color = "black"),
+    titlePanel("ETF走勢圖"),
+    fluidRow(
+      plotlyOutput("etfplot", "100%", "600px") %>% withSpinner(color = "white",type = 5)
+    ),
+    fluidRow(
+      column(3,
+             selectInput("etf.code","ETF代碼",choices = etf.code1),
+             hr(),
+             helpText("Made by renardbao"),
+             br()
+      ),
+      column(3,
+             checkboxGroupInput("select",
+                                label = "顯示",
+                                choices = c(1)
+             )
+             
+      ),
+      column(4,
+             sliderInput("date_range","時間範圍",
+                         min = Sys.Date(),max = Sys.Date(),
+                         value = c(Sys.Date(),Sys.Date())
+             )
+             
+      )
+    )
+  )
+  
+
+
+
+server <- function(input,output,session){
+  
+  
+  
+  observeEvent(input$etf.code,{         
+    updateCheckboxGroupInput(session,
+                             inputId  = "select",
+                             choices  = etf.data.melt[[input$etf.code]][,"variable"] %>% unique(),
+                             selected = etf.data.melt[[input$etf.code]][,"variable"] %>% unique()
+    )
+  })
+  
+  observeEvent(input$etf.code,{         
+    updateSliderInput(session,
+                      inputId  = "date_range",
+                      min = etf.data.melt[[input$etf.code]][,"date"] %>% min,
+                      max = etf.data.melt[[input$etf.code]][,"date"] %>% max,
+                      value = c( etf.data.melt[[input$etf.code]][,"date"] %>% min,
+                                 etf.data.melt[[input$etf.code]][,"date"] %>% max)
+    )
+  })
+  
+  
+  
+  
+  output$etfplot <- renderPlotly({
+    alpha.var <- ((etf.data.melt[[input$etf.code]][,"variable"] %>% unique() %in% input$select)*9+1)/10
+    
+    p<- 
+      ggplot(data = etf.data.melt[[input$etf.code]] %>% 
+               filter(date >= input$date_range[1]& date <= input$date_range[2]),
+             aes(label = value)) + #aes for plotly show value
+        geom_line(aes(x = date , y = scale_value ,color = variable,alpha = variable),
+                  size = 1.3) +
+        scale_alpha_manual(values = alpha.var) +
+        labs(title = input$etf.code %>% str_sub(4,nchar(input$etf.code)))+
+        theme(legend.key.width = unit(2,"cm"),
+              legend.text = element_text(size = 14,colour = "white"),
+              legend.key = element_rect(fill = 'transparent',color = "transparent"),
+              legend.title = element_text(size = 18,colou = "white"),
+              legend.background = element_rect(fill = "transparent" ),
+              axis.text.x = element_text(size = 24,face = "bold",color = "white"),
+              axis.title.x = element_text(size = 40,face = "bold",color = "white"),
+              axis.text.y = element_text(size = 24,face = "bold",color = "white"),
+              axis.title.y = element_text(size = 40,face = "bold",color = "white"),
+              panel.background = element_rect(fill = "black"),
+              plot.background = element_rect(fill = "black",color = "black"),
+              plot.title = element_text(hjust = 0.5,size = 40, face = "bold",color = "white"))
+    
+    ggplotly(p)
+  })
+  
+}
+
+
+
+
+
+shinyApp(ui = ui, server = server)
---
+++ etf_dashboard_1st/etf.R
@@ -0,0 +1,79 @@
+library(data.table)
+library(magrittr)
+library(reshape2)
+library(stringr)
+library(dplyr)
+library(urca)
+library(lubridate)
+library(shiny)
+library(shinyWidgets)
+library(plotly)
+library(shinycssloaders)
+options(scipen=999)
+etf <- fread("tetfp.csv",sep = ",",stringsAsFactors = F)
+names(etf) <- c("code","date","name","open","high","low","close","volume")
+etf$code %<>%  str_replace_all(" ","")
+#for data list query
+etf.code <- unique(etf$code) 
+#for ui choice list
+etf.code1 <- paste0("etf",etf.code)
+#台股指數
+twi <- fread("twii.csv",sep = ",",header = T,stringsAsFactors = F)
+names(twi) <- c("date" , "twi")
+twi$date %<>% as.Date(format="%Y-%m-%d")
+#美股指數
+sp500 <-  fread("sp500.csv",sep = ",",header = T,stringsAsFactors = F)
+names(sp500) <- c("date" , "sp500")
+sp500$date %<>% as.Date(format="%Y-%m-%d")
+#三種石油以及匯率
+oil <-  fread("petrol.csv",sep = ",",header = T,stringsAsFactors = F)
+oil$date %<>% as.Date(format="%Y-%m-%d") 
+names(oil)[5] <- "change.rate"
+oil <- arrange(oil,date)
+#多創一個變數只包含月份日期
+#用fast_strptime比較快
+etf$date %<>% as.character()%>% fast_strptime("%Y%m%d") %>% as.Date()
+etf$open %<>% as.numeric(2)
+etf$high %<>% as.numeric(2)
+etf$low %<>% as.numeric(2)
+etf$close %<>%  as.numeric(2)
+etf$volume %<>%  str_replace_all(",","") %>% as.numeric(2)
+
+# #完整資料串
+# etf.data <- list()
+# for (i in 1:length(etf.code)){
+#   etf.data[[paste0("etf",etf.code[i])]] <- 
+#     data.frame(
+#                open = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"open") %>% diff(),
+#                close = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"close") %>% diff(),
+#                high = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"high") %>% diff(),
+#                low =  merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>%
+#                  "["(,"low") %>% diff(),
+#                twi = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"twi") %>% diff() ,
+#                sp500 = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"sp500") %>% diff(),
+#                brent = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"brent") %>% diff(),
+#                change.rate = merge(etf %>% filter(code == etf.code[i]),sp500,by = "date") %>% 
+#                  merge(twi,by = "date") %>% merge(oil,by = "date") %>% 
+#                  "["(,"change.rate") %>% diff()
+#                
+#                )
+# }
+# 
+# 
+
+
+
+
